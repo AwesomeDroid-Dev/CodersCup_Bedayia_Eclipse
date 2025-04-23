cd35c9a1ba16b129771bb336b8a4ccd0fc12bc10
Noureddin Elazab <elazab.noureddin@gmail.com>
2025-04-23 20:45:16 +0200

    ai updates

--- Classes/AIPlayer.py
+++ Classes/AIPlayer.py
@@ -5,14 +5,20 @@ class AIPlayer(Player):
         super().__init__(x, y, width, height, "./Resources/Solid_red.png", speed=9)
         self.boots = None
         self.player = player
+        self.max_reaction = 60
+        self.reaction = 60
 
         self.type = "AIPlayer"
     
     def update(self, others, screen=None):
-        if self.pos.distance_to(self.player.pos) < 100:
-            self.attack_player(self.player)
-        else:
-            self.move_towards_player(self.player)
+        self.reaction -= 1
+        if self.reaction == 0:
+            if self.pos.distance_to(self.player.pos) < 100:
+                self.attack_player(self.player)
+            else:
+                self.move_towards_player(self.player)
+            self.reaction = self.max_reaction
+        
         return super().update(others, screen)
     
     def draw(self, screen):
